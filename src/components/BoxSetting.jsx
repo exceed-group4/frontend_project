bf5cb58826e40fff3f775baa24c37bd808d96a5d
FernPK <pornnapat5563@gmail.com>
2023-02-10 16:33:41 +0700

    boxSetting commit 1

--- src/components/BoxSetting.jsx
+++ src/components/BoxSetting.jsx
@@ -1,24 +1,120 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './BoxSetting.css'
+import './toggleButton.css'
 
-
-const BoxSetting = () => {
+const BoxSetting = (props) => {
+    const API_URL = 'http://group4.exceed19.online/safe_update'
+    const API_URL2 = 'http://group4.exceed19.online/password'
+    const {safeId2} = props;
+    const [pin2, setPin2] = useState("");
+    const [lock, setLock] = useState(true);
+    const [enable, setEnable] = useState(true);
+    const handleLock = () => {
+        setLock(!lock);
+    }
+    const handleEnable = () => {
+        setEnable(!enable);
+    }
+    const payload = {
+        "safe_id": safeId2,
+        "safe_pin": pin2,
+        "lock": lock,
+        "safe_system_available": enable
+    }
+    const payload2 = {
+        "safe_id": parseInt(safeId2),
+        "safe_pin": pin2
+    }
+    const checkPin = async () => {
+        const response = await fetch(API_URL2,{
+            method: 'PUT',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload2)
+        });
+        const data = await response.json();
+        // console.log(response);
+        if(response.status===200){
+            console.log(data);
+            alert(data.detail);
+            document.querySelector(".later").style.display = "block";
+        }
+        else{
+            console.log(data);
+            alert(data.detail);
+        }
+    }
+    const updateInfo = async () => {
+        const response = await fetch(API_URL,{
+            method: 'PUT',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+        const data = await response.json();
+        if(response.status===200){
+            console.log(data.detail);
+            alert(data.detail);
+        }
+        else{
+            console.log(data.detail);
+            alert(data.detail);
+        }
+    }
+    const handleSubmitSetting = (e) => {
+        e.preventDefault();
+        if(payload.safe_pin.length===6){
+            updateInfo();
+        }
+        else{
+            alert("Enter correct PIN.");
+        }
+    }
+    const handleSubmitPin = (e) => {
+        e.preventDefault();
+        if(payload.safe_pin.length===6){
+            checkPin();
+        }
+        else{
+            alert("Enter correct PIN.");
+        }
+    }
     return (
-        <div class="wrapper">
-            <div>Box Setting</div>
-
-            <div class="biggy">
-                <p class="texto">Safe Box System</p>
-
-                <div class="pin-wrapper">
+        <div className="wrapper2">
+            <div className="biggy">
+                <p className="texto">Safe Box System</p>
+                <p id="safeSafe">Safe Id: {safeId2}</p>
+                <div className="pin-wrapper">
                     <p>PIN:</p>
-                    <input id="pin" type="text" name="pin" />
-                    <button id="pin-submit">Submit</button>
+                    <input id="pin" type="password" name="pin" onChange={(e)=>setPin2(e.target.value)}/>
+                    <button id="pin-submit" type="submit" onClick={(e)=>handleSubmitPin(e)}>Submit</button>
                 </div>
-
-                <div class="button-wrapper">
-                    <button id="button-1">Button 1</button>
-                    <button id="button-2">Button 2</button>
+                <div className="later">
+                <div className="button-wrapper">
+                    <div>
+                        <p>Unlock</p>
+                        <label className="switch" id="switch1">
+                            <input type="checkbox" id="button-1" checked={lock} onChange={handleLock}/>
+                            <span class="slider round"></span>
+                        </label>
+                        <p>Lock</p>
+                    </div>
+                    <p id="secure">Security Setting</p>
+                    <div>
+                        <p>Unable</p>
+                        <label className="switch" id="switch2">
+                            <input type="checkbox" id="button-2" checked={enable} onChange={handleEnable}/>
+                            <span class="slider round"></span>
+                        </label>
+                        <p>Enable</p>
+                    </div>
+                    
+                </div>
+                <button type='submit' id='submitSetting' onClick={(e)=>handleSubmitSetting(e)}>Confirm</button>
                 </div>
             </div>
         </div>
