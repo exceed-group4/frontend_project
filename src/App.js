eda0a8622cfe81f50ec096d74a2dbc2a50d3f5c2
panitnt <panittatanya@gmail.com>
2023-02-10 12:07:02 +0700

    Already link with add box

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 import Home from './pages/Home';
+import Create from './components/Create';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path='' element={<Home/>}/>
         {/* <Route path='/safeBox/:id' element={}/> */}
-        <Route path='/addBox'/>
+        <Route path='/addBox' element={<Create/>}/>
       </Routes>
     </div>
   );
